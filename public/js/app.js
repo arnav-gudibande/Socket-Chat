2387604ef9177c3aec45d81f5100b38048c8b28b
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-11-19 12:10:50 -0800

    Integrated UI

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,10 @@ socket.on('connect', function() {
 
 socket.on('message', function(message) {
   console.log("New Message: " + message.text);
+  document.getElementById('smname').value = message.text;
 })
+
+function deploy() {
+  var chatMessage = document.getElementById('value').value;
+  socket.emit("message", { text: chatMessage });
+}
