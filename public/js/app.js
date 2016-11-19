4c56582c65c836299c09942ac1709745e5f88d37
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-11-18 21:54:04 -0800

    Implemented basic chat

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function() {
   console.log("Connected to socket.io server!");
 });
+
+socket.on('message', function(message) {
+  console.log("New Message: " + message.text);
+})
--- server.js
+++ server.js
@@ -6,8 +6,19 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on("message", function(message) {
+		console.log("Message Received: " + message.text);
+
+		socket.broadcast.emit("message", message)
+	});
+
+	socket.emit("message", {
+		text: "Welcome to the chat app!"
+	});
+
 });
 
 http.listen(PORT, function () {
